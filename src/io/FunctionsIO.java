edf360a5bd087b36482b729dc547e35d2fa532b1
Никита <veselov06na@gmail.com>
2025-11-10 15:31:02 +0400

    Добавлен пакет `io` и финальный класс `io.FunctionsIO`. Конструктор приватный и бросает `UnsupportedOperationException`, исключая наследование и создание экземпляров.

---
+++ src/io/FunctionsIO.java
@@ -0,0 +1,9 @@
+package io;
+
+public final class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("FunctionsIO is a utility class and cannot be instantiated");
+    }
+}
+
+
